84059a50c5c752ce00fd7cced819ac8173cb585b
manikantanuj <manikantanuj03@gmail.com>
2019-09-19 11:15:18 +0530

    Add files via upload
    Demo App for Spring Boot

---
+++ MyWebApp/src/main/java/com/anuj/demo/HomeController.java
@@ -0,0 +1,24 @@
+package com.anuj.demo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	@RequestMapping("home")
+	public ModelAndView home(@RequestParam("name")String name)
+	{
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name",name);
+		mv.setViewName("home");
+		return mv;
+	}
+
+}
+	
